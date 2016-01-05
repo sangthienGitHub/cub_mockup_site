6e38ae729e429095018216e616e08a3a4e09c395
Sang Tran <sangthien2010@gmail.com>
2016-01-05 13:16:33 -0600

    First one

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+
+ $(function() {
+   $("#slider1").responsiveSlides({
+        auto: true,
+        pager: true,
+        nav: true,
+        speed: 500,
+        maxwidth: 600,
+        namespace: "centered-btns"
+      });
+
+
+ });
+
